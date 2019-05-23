254f4a73c91a79cc0b6bcb939bb912fbc7245b97
jan <opilane@HP-CZC6297B9N>
2019-05-23 17:35:16 +0300

    Loo Superkangelase klass

---
+++ kangelased/kangelased/SuperKangelane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kangelased
+{
+    public class SuperKangelane : Kangelane
+    {
+        private double _Osavus;
+
+        public SuperKangelane(string Nimi, string Asukoht) : base(Nimi, Asukoht)
+        {
+            _Osavus = SingleRandom.Instance.Next(1, 60)/10.0; // arvud 1 kuni 5
+        }
+
+        public override int Päästa(int Ohustatuid)
+        {
+            return (int)Math.Round((95 + _Osavus) / 100 * Ohustatuid, 0);
+        }
+        public override string ToString()
+        {
+            return base.ToString()+" ja ta on selles "+_Osavus+" protsenti parem kui lihtsalt kangelased";
+        }
+    }
+}
